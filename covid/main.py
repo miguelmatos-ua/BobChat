424a7554927f9b515ae2c8aeb73062b8ae942be5
miguelmatos-ua <miguelcruzmatos@ua.pt>
2021-08-08 15:51:08 +0100

    initial

---
+++ covid/main.py
@@ -0,0 +1,162 @@
+"""
+Web scraper to get latest pdf and send telegram message with the pdf and its data
+
+@author: Miguel C. Matos
+"""
+import os
+import sys
+import telegram
+import requests
+import pdfplumber
+
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+def latest_day():
+    with open("last.txt") as last:
+        day = last.readlines()[-1]
+    return datetime.strptime(day, "%d/%m/%Y")
+
+
+def web_scrap(day):
+    """Find the latest pdf from Min. Saúde Website"""
+    uri = "https://covid19.min-saude.pt/relatorio-de-situacao/"
+    date=day.strftime("%Y%m%d")
+    with requests.get(uri) as r:
+        soup = BeautifulSoup(r.text, "lxml")
+
+        for a in soup.find_all("a"):
+            link = a.get("href")
+            if link and "DGS_boletim" in link and date in link and link.endswith(".pdf"):
+                return link
+
+
+
+def extract_data(pdf_link):
+    with open("today.pdf", "wb") as pdf_file:
+        r = requests.get(pdf_link)
+
+        for chunk in r.iter_content(2048):
+            pdf_file.write(chunk)
+        
+        r.close()
+
+    p = pdfplumber.open("today.pdf")
+    # first page data
+    page = p.pages[0]
+
+    # Crops 30% of the page
+    left_page = page.crop((0, 0, 0.3 * float(page.width), page.height))
+
+    # Split string by the line breaks
+    words_lst = left_page.extract_text().split("\n")
+    # Reverse the list
+    words_lst.reverse()
+
+    words_lst = list(filter(lambda x: x != " ", words_lst))  # remove empty spaces 
+    print(words_lst)
+
+    data = dict()
+    for i, s in enumerate(words_lst):
+        if "CONFIRMADOS" in ''.join(filter(lambda x: x != " ", s)):
+            new_data = [int('0' + ''.join([y for y in x if y.isnumeric()]))
+                        for x in words_lst[i + 1].split("+")]
+            if len(new_data) < 2:
+                new_data.append('0')
+            data['confirmados'] = {'total': new_data[0], 'novos': new_data[1]}
+        elif "ÓBITOS" in ''.join(filter(lambda x: x != " ", s)):
+            new_data = [int('0' + ''.join([y for y in x if y.isnumeric()]))
+                        for x in words_lst[i + 1].split("+")]
+            if len(new_data) < 2:
+                new_data.append('0')
+            data['óbitos'] = {'total': new_data[0], 'novos': new_data[1]}
+        elif "RECUPERADOS" in ''.join(filter(lambda x: x != " ", s)):
+            new_data = [int('0' + ''.join([y for y in x if y.isalnum()]))
+                        for x in words_lst[i + 1].split("+")]
+            if len(new_data) < 2:
+                new_data.append('0')
+            data['recuperados'] = {'total': new_data[0], 'novos': new_data[1]}
+
+    # Aveiro stats
+    is_aveiro = True
+    try:
+        page = p.pages[3]
+    except IndexError:
+        is_aveiro = False
+    
+    if is_aveiro:
+        aveiro = None
+        # iterate to find Aveiro stats
+        for lines in page.extract_text().split("\n"):
+            if "Aveiro" in lines:
+                line = lines.split(" ")
+                aveiro = line[line.index("Aveiro") + 1]
+                break
+        
+        data["aveiro"] = aveiro
+
+    # Internados values
+    page = p.pages[0]
+    internados_page = page.crop((0, 0.84 * float(page.height), 0.2 * float(page.width), 0.9 * float(page.height)))
+    uci_page = page.crop((0.22 * float(page.width), 0.84 * float(page.height), page.width, 0.9 * float(page.height)))
+
+    data["internados"] = __extract_vals(internados_page)
+    data["uci"] = __extract_vals(uci_page)
+
+    return data
+
+
+def __extract_vals(page):
+    print([x for x in page.extract_text().split("\n") if x != " "])
+    vals = [x for x in page.extract_text().split("\n") if x != " "][0]
+    char = "-" if "-" in vals else "+"
+    m = [x.replace(" ", "") for x in vals.split(char)]
+    if m[1] == '':
+        m[1] = "0"
+    return int(m[0]), char + m[1]
+
+
+def send_msg(data, day, file=None):
+    aveiro = data.get("aveiro")
+    aveiro_string = '' if aveiro is None else f'\n\nAveiro: {aveiro}'
+    internados = data.get("internados")
+    uci = data.get("uci")
+
+    internados_str = ""
+    if uci is not None and internados is not None:
+        internados_str = f"\n\nInternados: {internados[0]} | {internados[1]}\n" \
+                         f"UCI: {uci[0]} | {uci[1]}"
+
+    msg = f"<b>{day}</b>\n" \
+          f"Novos Casos: {data['confirmados']['total']} | +{data['confirmados']['novos']}\n" \
+          f"Óbitos: {data['óbitos']['total']} | +{data['óbitos']['novos']}\n" \
+          f"Recuperados: {data['recuperados']['total']} | +{data['recuperados']['novos']}" \
+          + internados_str + aveiro_string
+    
+    # TODO: Send the message with the pdf document
+
+
+def update_last():
+    with open("last.txt", "w") as last:
+        last.write(datetime.today().strftime("%d/%m/%Y"))
+
+
+if __name__ == "__main__":
+    BOT_TOKEN = sys.argv[1]
+    CHAT_ID = sys.argv[2]
+
+    bot = telegram.Bot(BOT_TOKEN)
+    last = latest_day()
+
+    if datetime.today() == last:
+        # message already sent. end program
+        print("Message already sent today")
+        sys.exit(0)
+    
+    # Message hasn't been sent yet
+    today = datetime.today()
+    link = web_scrap(today)
+    data = extract_data(link)
+    send_msg(data, today.strftime("%d/%m/%Y"), file=link)
+    update_last()
---
+++ covid/test.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+from main import *
+
+
+def test_latest_day1():
+    """Assert latest day is equal to today or yesterday"""
+    today = datetime.today()
+    yesterday = today - timedelta(days=1)
+
+    last = latest_day().strftime("%d/%m/%Y")
+    assert last == today.strftime("%d/%m/%Y") or last == yesterday.strftime("%d/%m/%Y")
+
+
+def test_web_scrap1():
+    """Assert that a real document does not return none"""
+    link = web_scrap(datetime(2021, 8, 7))
+
+    assert link is not None
+    assert link == "https://covid19.min-saude.pt/wp-content/uploads/2021/08/523_DGS_boletim_20210807.pdf"
+
+
+def test_web_scrap2():
+    """Assert that a not real document return none"""
+    link = web_scrap(datetime(1999, 8, 6))
+    assert link is None
+
+
+def test_extract_data1():
+    link = "https://covid19.min-saude.pt/wp-content/uploads/2021/08/523_DGS_boletim_20210807.pdf"
+    data = {
+        "confirmados": {
+            "total": 984985,
+            "novos": 2621,
+        },
+        "óbitos": {
+            "total": 17457,
+            "novos": 17,
+        },
+        "recuperados": {
+            "total": 923510,
+            "novos": 3232,
+        },
+        "internados": (838, "-28"),
+        "uci": (186, "-8")
+    }
+
+    assert extract_data(link) == data
+
+
+def test_extract_data2():
+    link = "https://covid19.min-saude.pt/wp-content/uploads/2021/08/523_DGS_boletim_20210807.pdf"
+    data = {
+        "confirmados": {
+            "total": 984985,
+            "novos": 2621,
+        },
+        "óbitos": {
+            "total": 17457,
+            "novos": 17,
+        },
+        "recuperados": {
+            "total": 923510,
+            "novos": 3232,
+        },
+        "internados": (838, "-28"),
+        "uci": (186, "-8")
+    }
+
+    assert extract_data(link) == data
