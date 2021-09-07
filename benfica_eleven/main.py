6dd1e44eefa77913a30c9b546b6ad7e100d54930
Miguel Matos <miguelcruzmatos@ua.pt>
2021-09-07 14:17:41 +0100

    init benfica eleven

---
+++ benfica_eleven/main.py
@@ -0,0 +1,21 @@
+import os
+
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+
+def get_user_tweets():
+    ...
+
+
+def check_eleven_is_out():
+    ...
+
+
+def send_tweet_message():
+    ...
+
+
+if __name__ == "__main__":
+    ...
