67fc1a971a1fc8b0dd1c0391b77050542cc8af8b
Miguel Matos <miguelcruzmatos@ua.pt>
2021-09-07 14:52:36 +0100

    add twitter secrets

--- benfica_eleven/main.py
+++ benfica_eleven/main.py
@@ -3,6 +3,9 @@ import os
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
+TWITTER_API_KEY = os.environ["TWITTER_API_KEY"]
+TWITTER_SECRET = os.environ["TWITTER_SECRET"]
+TWITTER_BEARER_TOKEN = os.environ["TWITTER_BEARER_TOKEN"]
 
 
 def get_user_tweets():
