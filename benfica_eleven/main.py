4caa41d165f7306c2282e52a73e9203bd9e8a7e2
Miguel Matos <miguelcruzmatos@ua.pt>
2021-09-07 16:18:51 +0100

    add get user tweets method

--- benfica_eleven/main.py
+++ benfica_eleven/main.py
@@ -1,22 +1,38 @@
 import os
-
+import requests
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
-TWITTER_API_KEY = os.environ["TWITTER_API_KEY"]
-TWITTER_SECRET = os.environ["TWITTER_SECRET"]
 TWITTER_BEARER_TOKEN = os.environ["TWITTER_BEARER_TOKEN"]
 
 
-def get_user_tweets():
+def get_user_tweets(user=21390437):
+    """Get user Tweets
+
+    Returns the 100 most recent tweets of a specific user.
+
+    Args:
+        user (int, optional): Id of the user. Defaults to 21390437.
+
+    Returns:
+        dict, None: Most recent tweets
+    """
+    uri = f"https://api.twitter.com/2/users/{user}/tweets"
+    headers = {
+        "Authorization": f"Bearer {TWITTER_BEARER_TOKEN}"
+    }
+
+    with requests.get(uri, headers=headers) as r:
+        j: dict = r.json()
+
+    return j.get("data")
+
+
+def check_eleven_is_out(tweets):
     ...
 
 
-def check_eleven_is_out():
-    ...
-
-
-def send_tweet_message():
+def send_tweet_message(tweet):
     ...
 
 
