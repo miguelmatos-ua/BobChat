89a2d35e462d8542bbac2f76b071d37f784d8d80
Miguel Matos <miguelcruzmatos@ua.pt>
2021-09-07 16:32:41 +0100

    send tweet message

--- benfica_eleven/main.py
+++ benfica_eleven/main.py
@@ -13,12 +13,12 @@ def get_user_tweets(user=21390437):
     Returns the 100 most recent tweets of a specific user.
 
     Args:
-        user (int, optional): Id of the user. Defaults to 21390437.
+        user (int, optional): Id of the user. Defaults to 21390437 (@SLBenfica account).
 
     Returns:
-        dict, None: Most recent tweets
+        (list, None): Most recent tweets
     """
-    uri = f"https://api.twitter.com/2/users/{user}/tweets"
+    uri = f"https://api.twitter.com/2/users/{user}/tweets?max_results=100"
     headers = {
         "Authorization": f"Bearer {TWITTER_BEARER_TOKEN}"
     }
@@ -32,24 +32,26 @@ def get_user_tweets(user=21390437):
 def check_eleven_is_out(tweets):
     for tweet in tweets:
         if "JÁ HÁ ONZE" in tweet["text"]:
-            return tweet
-    
+            return tweet["text"]
+
     return None
 
 
 def send_tweet_message(tweet):
-    ...
+    telegram_uri = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
+    return requests.get(telegram_uri.format(BOT_TOKEN, CHAT_ID, tweet["text"]))
 
 
 if __name__ == "__main__":
     user_tweets = get_user_tweets()
     if not user_tweets:
-        sys.exit(0)
+        print("There are no user tweets")
+        sys.exit(1)
 
     lineup_tweet = check_eleven_is_out(user_tweets)
-    
-    if not lineup_tweet:
-        sys.exit(0)
-    
-    send_tweet_message(lineup_tweet)
 
+    if not lineup_tweet:
+        print("There is not lineup tweet")
+        sys.exit(0)
+
+    print(send_tweet_message(lineup_tweet))
