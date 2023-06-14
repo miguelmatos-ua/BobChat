f34f6f41f071cf92b71a0e14d599e0166ec536aa
Miguel C. Matos <miguelcruzmatos@ua.pt>
2023-06-14 16:18:14 +0100

    Add weather forecast workflow

---
+++ weather/main.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import telegram
+
+# load environmental variables
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("The value of BOT_TOKEN is not defined")
+CHAT_ID = os.environ.get("CHAT_ID")
+if not CHAT_ID:
+    raise ValueError("The value of the CHAT_ID is not defined")
+
+bot = telegram.Bot(BOT_TOKEN)
+
+
+def main():
+    """fetch weather for Aveiro"""
+    img_name = "t.png"
+    with requests.get("https://wttr.in/Aveiro.png?1", stream=True) as r:
+        with open(img_name, "wb") as im:
+            for chunk in r.iter_content(1024):
+                im.write(chunk)
+    bot.send_photo(chat_id=CHAT_ID, photo=open(img_name, "rb"))
+
+
+if __name__ == "__main__":
+    main()
