4962655780e92a3830b41965983b1e42452864c3
Miguel C. Matos <miguelcruzmatos@ua.pt>
2023-06-15 12:04:13 +0100

    removes unneccessary file after execution

--- weather/main.py
+++ weather/main.py
@@ -14,7 +14,7 @@ bot = telegram.Bot(BOT_TOKEN)
 
 
 def main():
-    """fetch weather for Aveiro"""
+    """Fetch weather for Aveiro"""
     img_name = "t.png"
     with requests.get("https://wttr.in/Aveiro.png?1", stream=True) as r:
         with open(img_name, "wb") as im:
