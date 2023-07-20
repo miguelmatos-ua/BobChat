f63f96bdfb48fed5e00f17a19674fe0bada51eea
Miguel C. Matos <miguelcruzmatos@ua.pt>
2023-07-20 14:42:11 +0100

    retry sending the weather

--- weather/main.py
+++ weather/main.py
@@ -1,6 +1,8 @@
 import requests
 import os
 import telegram
+import sys
+import time
 
 # load environmental variables
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
@@ -16,11 +18,20 @@ bot = telegram.Bot(BOT_TOKEN)
 def main():
     """Fetch weather for Aveiro"""
     img_name = "t.png"
-    with requests.get("https://wttr.in/Aveiro.png?1&m", stream=True) as r:
-        with open(img_name, "wb") as im:
-            for chunk in r.iter_content(1024):
-                im.write(chunk)
-    bot.send_photo(chat_id=CHAT_ID, photo=open(img_name, "rb"))
+    for i in range(5):
+        try:
+            print("Attempt", i + 1)
+            with requests.get("https://wttr.in/Aveiro.png?1&m", stream=True) as r:
+                with open(img_name, "wb") as im:
+                    for chunk in r.iter_content(1024):
+                        im.write(chunk)
+            bot.send_photo(chat_id=CHAT_ID, photo=open(img_name, "rb"))
+            break
+        except Exception as e:
+            print("error sending photo:", e)
+            time.sleep(1)
+    else:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
